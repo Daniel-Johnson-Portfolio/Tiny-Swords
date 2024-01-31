54a2f9d34010d46b4fcd66e041d19d7c5808b9dc
Daniel-Johnson-Portfolio <dannyj0207@gmail.com>
2024-01-31 14:25:21 +0000

    nothing

--- Assets/Assets/Scripts/ScriptableObjects/SCR_PlayerSO.cs
+++ Assets/Assets/Scripts/ScriptableObjects/SCR_PlayerSO.cs
@@ -5,15 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/Eenmies", order = 1)]
 public class SCR_PlayerSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
