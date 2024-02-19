24228ceb94e450c27901f173c9c7fad3dec8f7dd
Daniel-Johnson-Portfolio <dannyj0207@gmail.com>
2024-02-19 09:15:54 +0000

    Ligting 2

--- Assets/Assets/Scripts/AI/SCR_AI_MELEE.cs
+++ Assets/Assets/Scripts/AI/SCR_AI_MELEE.cs
@@ -15,10 +15,5 @@ public class SCR_AI_Melee : SCR_AI_CLASS
     protected override void Update()
     {
         base.Update();
-
-        if (base.agent.velocity.magnitude > 0) 
-        {
-            gameObject.GetComponent<Light2D>().lightType
-        }
     }
 }
